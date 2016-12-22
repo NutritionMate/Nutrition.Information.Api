a20b920dd535b3d52591ff1fb09253672e231567
Rowan Walker <rowanjkw@gmail.com>
2016-12-22 23:20:59 +1300

    https://github.com/NutritionMate/Nutrition.Information.Api/issues/14 Filling out the unit test index.js to perform 100% coverage to test we can merge a Pull Request given it has 100% unit test coverage.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
 var index = require('../');
  
 describe('index.js', function() {
  it('returns hello world', function() {
-   
+ 	expect(index()).to.equal('hello world');
  });
 });
